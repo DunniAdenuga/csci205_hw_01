95594ea91c8eff47269b5ade260c59b379850d1e
Tim Woodford <tww014@bucknell.edu>
2015-10-07 08:33:10 -0400

    Clean up the ToneGenerator code

--- src/hw01/tone/ToneGenerator.java
+++ src/hw01/tone/ToneGenerator.java
@@ -16,12 +16,10 @@
 package hw01.tone;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -31,66 +29,33 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  * @author tww014
  */
 public class ToneGenerator {
-    /*/**
+    /**
+     * Play a tone. Code for opening the <code>SourceDataLine</code> is based on
+     * code from www.wolinlabs.com.
      *
-     * @param args
-     * @throws LineUnavailableException
+     * @see http://www.wolinlabs.com/blog/java.sine.wave.html
+     * @param args Command line args
      * @author Tim Woodford
      * @throws java.io.IOException
      * @throws javax.sound.sampled.UnsupportedAudioFileException
+     * @throws javax.sound.sampled.LineUnavailableException
      */
-
     public static void main(String[] args) throws LineUnavailableException, IOException, UnsupportedAudioFileException, InterruptedException {
-        //Clip output = AudioSystem.getClip();
-        //output.open(AudioSystem.getAudioInputStream(new File("0001.wav")));
-        SourceDataLine output = AudioSystem.getSourceDataLine(new AudioFormat(
-                44100, 16, 1, true, true));
         Tone tone = new SineTone(440, 1.0f);
-        output.start();
-        //tone.writeLoop(output);
-        //Thread.sleep(4000);
-        output.drain();
-        output.close();
 
-        final int SAMPLING_RATE = 44100;            // Audio sampling rate
-        final int SAMPLE_SIZE = 2;                  // Audio sample size in bytes
-
-        SourceDataLine line;
-        double fFreq = 440;                         // Frequency of sine wave in hz
-
-        //Position through the sine wave as a percentage (i.e. 0 to 1 is 0 to 2*PI)
-        double fCyclePosition = 0;
-        //Open up audio output, using 44100hz sampling rate, 16 bit samples, mono, and big 
-        // endian byte ordering
-        AudioFormat format = new AudioFormat(SAMPLING_RATE, 16, 1, true, true);
+        AudioFormat format = new AudioFormat(44100, 16, 1, true, true);
         DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
 
-        if (!AudioSystem.isLineSupported(info)) {
-            System.out.println("Line matching " + info + " is not supported.");
-            throw new LineUnavailableException();
-        }
-
-        line = (SourceDataLine) AudioSystem.getLine(info);
-        line.open(format);
-        line.start();
-        line.addLineListener(new LineListener() {
-            @Override
-            public void update(LineEvent event) {
+        try (SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info)) {
+            line.open(format);
+            line.start();
+            line.addLineListener((LineEvent event) -> {
                 System.out.println(event);
-            }
-        });
+            });
 
-        // Make our buffer size match audio system's buffer
-        ByteBuffer cBuf = ByteBuffer.allocate(line.getBufferSize());
+            tone.writeLoop(line);
 
-        int ctSamplesTotal = (int) (format.getFrameRate() * 5);         // Output for roughly 5 seconds
-        float sampleRate = line.getFormat().getFrameRate();
-
-        tone.writeLoop(line);
-
-      //Done playing the whole waveform, now wait until the queued samples finish 
-        //playing, then clean up and exit
-        line.drain();
-        line.close();
+            line.drain();
+        }
     }
 }
