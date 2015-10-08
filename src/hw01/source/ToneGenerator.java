8bf46a995c3a9105110b185eb05d31b6deb8cb6c
Timothy Woodford <timthewoodford@gmail.com>
2015-10-07 23:34:00 -0400

    Remove old tone output method

--- src/hw01/source/ToneGenerator.java
+++ src/hw01/source/ToneGenerator.java
@@ -15,17 +15,13 @@
  */
 package hw01.source;
 
-import java.io.File;
 import java.io.IOException;
-import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
