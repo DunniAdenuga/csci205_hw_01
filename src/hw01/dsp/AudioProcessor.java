c766142b9d2bdfcbe55f00768a8720501989b2af
Tim Woodford <tww014@bucknell.edu>
2015-10-08 17:16:02 -0400

    Fix major bug in buffer handling by using nio's ByteBuffer
    Note that this means that we can't handle 24-bit audio anymore

--- src/hw01/dsp/AudioProcessor.java
+++ src/hw01/dsp/AudioProcessor.java
@@ -18,8 +18,10 @@ package hw01.dsp;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 
 /**
  *
@@ -34,56 +36,46 @@ public abstract class AudioProcessor extends FilterInputStream {
     public AudioProcessor(InputStream in, AudioFormat format) {
         super(in);
         this.format = format;
-        bitScale = (int) (1L << format.getSampleSizeInBits() - 1);
-    }
-
-    /**
-     * Write a certain number of bytes to the output stream
-     *
-     * This is loosely based on code from the JPEG encoder in Piston's rust
-     * image library.
-     *
-     * @see
-     * https://github.com/PistonDevelopers/image/blob/master/src/jpeg/encoder.rs
-     *
-     * @param b The byte array to write to
-     * @param position The position at which to start
-     * @param data The data to write
-     * @param length The number of bytes to write
-     */
-    public static void writeBytes(byte[] b, int position, int data, int length) {
-        int start = length * position;
-        for (int i = length - 1; i >= 0; i--) {
-            b[start + i] = (byte) (data & 0xff);
-            data >>= 8;
-        }
+        bitScale = (int) ((1L << format.getSampleSizeInBits() / format.getChannels()) - 1);
     }
 
     protected float[] collapse(byte[] b, int off, int len) {
-        final int frameSize = format.getFrameSize();
-        int total = len / frameSize;
-        float[] ret = new float[total];
+        final int frameSize = format.getFrameSize() / format.getChannels();
+        final int total = len / frameSize;
+        final float[] ret = new float[total];
+        final ByteBuffer buf = ByteBuffer.wrap(b);
         for (int i = 0; i < total; i++) {
             int curr = 0;
-            // Necessary for two's complement...
-            if ((b[frameSize * i] & (1 << 8)) != 0) {
-                curr = 0xffffffff;
+            switch (frameSize) {
+                case 1:
+                    ret[i] = buf.get() / (float) bitScale;
+                    break;
+                case 2:
+                    ret[i] = buf.getShort() / (float) bitScale;
+                    break;
+                default:
+                    throw new UnsupportedOperationException();
             }
-            for (int j = 0; j < frameSize; j++) {
-                curr <<= 8;
-                curr ^= b[frameSize * i + j] & 0xff;
-            }
-            ret[i] = curr / (float) bitScale;
         }
         return ret;
     }
 
     protected void expand(byte[] b, int off, int len, float[] data) {
-        final int frameSize = format.getFrameSize();
+        final int frameSize = format.getFrameSize() / format.getChannels();
         int total = len / frameSize;
+        ByteBuffer buf = ByteBuffer.wrap(b);
         for (int i = 0; i < total; i++) {
             int curr = (int) (data[i] * bitScale);
-            writeBytes(b, i, curr, frameSize);
+            switch (frameSize) {
+                case 1:
+                    buf.put((byte) curr);
+                    break;
+                case 2:
+                    buf.putShort((short) curr);
+                    break;
+                default:
+                    throw new UnsupportedOperationException();
+            }
         }
     }
 
@@ -101,6 +93,10 @@ public abstract class AudioProcessor extends FilterInputStream {
             index += len;
         }
         return len;
+    }
+
+    public AudioInputStream getAudioStream(int samples) {
+        return new AudioInputStream(this, this.format, samples);
     }
 
     protected abstract void processAudio(float[] audioData);
