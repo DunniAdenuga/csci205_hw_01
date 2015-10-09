3f593a0dcff348fcbc677a8a2f44189b94416d15
Dunni Adenuga <ia005@bucknell.edu>
2015-10-09 11:02:39 -0400

    "HW 1 Performing most functions-  more modular"

--- src/hw01/tone/UserInterface.java
+++ src/hw01/tone/UserInterface.java
@@ -16,14 +16,20 @@
  */
 package hw01.tone;
 
+import hw01.dsp.AudioProcessor;
+import hw01.dsp.ConvolutionBuiler;
+import hw01.dsp.VolumeControl;
 import hw01.source.SawtoothTone;
 import hw01.source.SineTone;
 import hw01.source.SquareTone;
 import hw01.source.Tone;
 import hw01.source.TriangleTone;
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  *
@@ -34,7 +40,14 @@ public class UserInterface {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
+    /**
+     * Start off
+     *
+     * @param args
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws IOException, InterruptedException, UnsupportedAudioFileException {
         Tone tone = null;
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to THE TONE");
@@ -49,12 +62,19 @@ public class UserInterface {
 
             existing();
         }
+
     }
 
-    public static void existing() throws InterruptedException, IOException {
+    /**
+     * If audio exists in memory, read in
+     *
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    public static void existing() throws InterruptedException, IOException, UnsupportedAudioFileException {
         Scanner input = new Scanner(System.in);
-        String wavFile = null;
-        AudioInputStream finalResult = null;
+        String wavFile;
+
         System.out.print("Please Enter Wave File: ");
         wavFile = input.next();
         try {
@@ -65,28 +85,23 @@ public class UserInterface {
                     "Information about your WaveForm is printed below! ");
 
             WavePlay.display(wavFile);
-            System.out.print("Do you want to downsample the tone y | n?");
-            if (input.next().equals("y")) {
-                System.out.print(
-                        "Please Enter the degree of percentage in percent: ");
-                int deg = Integer.parseInt(input.next());
-                finalResult = WavePlay.downsample(wavFile, deg);
 
-                System.out.print(
-                        "Give a name for the file to save the downsampled version: ");
-                String newFile = input.next();
-                WavePlay.saveWav(finalResult, newFile);
-                System.out.println("Playing downsampled file...");
-                WavePlay.playFile(newFile);
-
-            }
         } catch (IllegalArgumentException e) {
             System.out.print("Audio File not supported.");
         }
+        downsampleFile(wavFile);
+        delayFile(wavFile);
+        volumeFile(wavFile, 5);
+
     }
 
+    /**
+     * Generate new tone and work with it
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static void generate() throws IOException, InterruptedException {
-        AudioInputStream finalResult = null;
         Scanner input = new Scanner(System.in);
         Tone tone = null;
         int time = 5;
@@ -111,36 +126,114 @@ public class UserInterface {
         if (typeTone.equals("4")) {
             tone = new TriangleTone(freq, amp);
         }
-        System.out.print("Do you want to play the Tone ? If yes enter 1: ");
-        String play = input.next();
-        if (play.equals("1")) {
-            System.out.print("Please enter the amount of time for playing: ");
-            time = Integer.parseInt(input.next());
-            WavePlay.playFile(tone, time);
-        }
+
+        System.out.print("Please enter the amount of time for playing: ");
+        time = Integer.parseInt(input.next());
+        WavePlay.playFile(tone, time);
+
         System.out.println(
                 "Information about your WaveForm is printed below! ");
         WavePlay.display(new AudioInputStream(tone.getInputStream(),
                                               tone.getFormat(), time));
-        System.out.print("Do you want to downsample the tone y | n?");
-        if (input.next().equals("y")) {
-            System.out.print(
-                    "Please Enter the degree of percentage in percent: ");
-            int deg = Integer.parseInt(input.next());
-            finalResult = WavePlay.downsample((new AudioInputStream(
-                                               tone.getInputStream(),
-                                               tone.getFormat(), time)), deg);
-            System.out.print(
-                    "Give a name for the file to save the downsampled version: ");
-            String newFile = input.next();
-            WavePlay.saveWav(finalResult, newFile);
-            System.out.println("Playing downsampled file...");
-            WavePlay.playFile(newFile);
-            System.out.print(
-                    "Do you want to add a single delay/echo to sound ? If yes enter y: ");
-            if (input.next().equals("y")) {
-                System.out.print("Enter Delay Time: ");
-            }
-        }
+        downsampleTone(tone, typeTone);
+        delayTone(tone, time);
+        volumeTone(tone, time);
+
+    }
+
+    public static void downsampleFile(String wavfile) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        String wavFile = null;
+        AudioInputStream finalResult = null;
+        System.out.print(
+                "Please Enter the degree of downsampling in percent: ");
+        int deg = Integer.parseInt(input.next());
+        finalResult = WavePlay.downsample(wavFile, deg);
+
+        System.out.print(
+                "Give a name for the file to save the downsampled version: ");
+        String newFile = input.next();
+        WavePlay.saveWav(finalResult, newFile);
+        System.out.println("Playing downsampled file...");
+        WavePlay.playFile(newFile);
+    }
+
+    public static void downsampleTone(Tone tone, String typeTone) throws IOException, InterruptedException {
+        Tone finalResult = null;
+        Scanner input = new Scanner(System.in);
+
+        int time = 5;
+        System.out.print(
+                "Please Enter the degree of downsample in percent: ");
+        double deg = Double.parseDouble(input.next());
+        finalResult = WavePlay.downsample(tone, typeTone, time, deg);
+        System.out.print(
+                "Give a name for the file to save the downsampled version: ");
+        String newFile = input.next();
+        WavePlay.saveWav(new AudioInputStream(finalResult.getInputStream(),
+                                              finalResult.getFormat(), time),
+                         newFile);
+        System.out.println("Playing downsampled file...");
+        WavePlay.playFile(finalResult, time);
+    }
+
+    public static void delayTone(Tone tone, int time) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Delay Time: ");
+        float delay = Float.parseFloat(input.next());
+        System.out.print("Enter delay mutiplier: ");
+        float delayM = Float.parseFloat(input.next());
+        System.out.print("How Long do you want it played for ? ");
+        AudioProcessor proc = ConvolutionBuiler.simpleDelay(
+                new AudioInputStream(tone.getInputStream(),
+                                     tone.getFormat(), time), delay,
+                delayM);
+        System.out.print("Playing delayed file...");
+        WavePlay.playFile(proc.getAudioStream(44100 * time));
+        System.out.print("Delay File saved in delayFile.wav ");
+        WavePlay.saveWav(proc.getAudioStream(44100 * time), "delayFile.wav");
+    }
+
+    public static void delayFile(String wavFile) throws UnsupportedAudioFileException, InterruptedException, IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a delay time: ");
+        float delay = Float.parseFloat(input.next());
+        System.out.print("Enter delay mutiplier: ");
+        float delayM = Float.parseFloat(input.next());
+        System.out.print("How Long do you want it played for ? ");
+        int time = Integer.parseInt(input.next());
+        AudioProcessor proc = ConvolutionBuiler.simpleDelay(
+                AudioSystem.getAudioInputStream(new File(wavFile)), delay,
+                delayM / 100);
+        System.out.print("Playing delayed file...");
+        WavePlay.playFile(proc.getAudioStream(44100 * time));
+        System.out.print("Delay File saved.");
+        WavePlay.saveWav(proc.getAudioStream(44100 * time), "delayFile.wav");
+    }
+
+    public static void volumeTone(Tone tone, int time) throws InterruptedException, IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a percentage volume adjuster: ");
+        float volumeAdjust = Float.parseFloat(input.next());
+        AudioInputStream newAudio = new VolumeControl(new AudioInputStream(
+                tone.getInputStream(), tone.getFormat(), time), volumeAdjust).getAudioStream(
+                        44100 * time);
+        System.out.print("Playing volume adjusted file...");
+        WavePlay.playFile(newAudio);
+        System.out.print("Volume Adjusted File saved in volumeAdjusted.wav");
+        WavePlay.saveWav(newAudio, "volumeAdjusted.wav");
+    }
+
+    public static void volumeFile(String wavFile, int time) throws UnsupportedAudioFileException, InterruptedException, IOException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a percentage volume adjuster: ");
+        float volumeAdjust = Float.parseFloat(input.next());
+        AudioInputStream newAudio = new VolumeControl(
+                AudioSystem.getAudioInputStream(new File(wavFile)), volumeAdjust).getAudioStream(
+                        44100 * time);
+        System.out.print("Playing volume adjusted file...");
+        WavePlay.playFile(newAudio);
+        System.out.print("Volume Adjusted File saved in volumeAdjusted.wav.");
+        WavePlay.saveWav(newAudio, "volumeAdjusted.wav");
     }
 }
