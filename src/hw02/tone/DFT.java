07b2269224e9c4e625fdaeab9bd88e69a519803b
Dunni Adenuga <ia005@bucknell.edu>
2015-10-19 08:46:36 -0400

    "stuff"

--- src/hw02/tone/DFT.java
+++ src/hw02/tone/DFT.java
@@ -21,6 +21,7 @@ import hw02.source.SineTone;
 import hw02.source.Tone;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import javax.sound.sampled.AudioInputStream;
 
 /**
@@ -35,16 +36,24 @@ class DFTException extends Exception {
 
 /**
  *
- * @author ia005
+ * @author Dunni Adenuga
  * @see http://introcs.cs.princeton.edu/java/97data/FFT.java.html
  * @see
  * http://www.nayuki.io/res/how-to-implement-the-discrete-fourier-transform/Dft.java
  */
 public class DFT {
     public static void main(String[] args) throws DFTException, IOException {
-        getPeak();
+        Tone tone = new SineTone(600, (float) 0.95);
+        getPeak(tone, 5);
     }
 
+    /**
+     * Perform Fast Fourier
+     *
+     * @param x - input
+     * @return
+     * @throws DFTException
+     */
     public static Complex[] computeFFT(Complex[] x) throws DFTException {
         int N = x.length;
 
@@ -55,17 +64,17 @@ public class DFT {
             throw new DFTException("array length is not a power of 2");
         }
         //divide and conquer- fft for real and imaginary
-        Complex[] odd = new Complex[N / 2];
-        for (int i = 0; i < N / 2; i++) {
-            odd[i] = x[(2 * i) + 1];
-        }
-        Complex[] result1 = computeFFT(odd);
-
         Complex[] even = new Complex[N / 2];
         for (int i = 0; i < N / 2; i++) {
             even[i] = x[(2 * i)];
         }
         Complex[] result2 = computeFFT(even);
+
+        Complex[] odd = new Complex[N / 2];
+        for (int i = 0; i < N / 2; i++) {
+            odd[i] = x[(2 * i) + 1];
+        }
+        Complex[] result1 = computeFFT(odd);
 
         //combine
         //[Re(x(t))cos(2πtk/n)+Im(x(t))sin(2πtk/n)]+[−Re(x(t))sin(2πtk/n)+Im(x(t))cos(2πtk/n)]i.
@@ -82,20 +91,21 @@ public class DFT {
         return answer;
     }
 
-    //read into memory in bytes
     /**
+     * Read tone into memory in a bytes array
+     *
+     * @param tone - tone to be read in
+     * @return - array of bytes
      * @see http://www.jsresources.org/examples/AudioDataBuffer.java.html
      */
-    public static byte[] tryStuff() throws IOException {
+    public static byte[] getByteArray(Tone tone) throws IOException {
 
-        Tone tone = new SineTone(440, (float) 0.95);
-        AudioInputStream toneStream = tone.getAudioInputStream(5);
+        AudioInputStream toneStream = tone.getAudioInputStream(1);
         ByteArrayOutputStream bae = new ByteArrayOutputStream();
         int nBufferSize = 1024 * tone.getAudioFormat().getFrameSize();
-
-        System.out.println(nBufferSize);
-        System.out.println(nBufferSize
-                           / 1024);
+        //System.out.println(tone.getAudioFormat().getSampleRate());
+        //System.out.println(nBufferSize);
+        //System.out.println(nBufferSize/ 1024);
 
         byte[] stuff = new byte[nBufferSize];
         while (true) {
@@ -105,53 +115,61 @@ public class DFT {
             }
             bae.write(stuff, 0, nBytesRead);
         }
-        byte[] toneData = bae.toByteArray();
-        return toneData;
+        //byte[] toneData = bae.toByteArray();
+        return stuff;
     }
 
-    /**
-     *
-     * @param byteArray
-     * @see
-     * http://stackoverflow.com/questions/15533854/converting-byte-array-to-double-array
-     * @return
-     */
-    /**
-     * public static double[] toDoubleArray(byte[] byteArray) { int times =
-     * Double.SIZE / Byte.SIZE; double[] doubles = new double[byteArray.length /
-     * times]; for (int i = 0; i < doubles.length; i++) { doubles[i] =
-     * ByteBuffer.wrap(byteArray, i * times, times).getDouble(); } return
-     * doubles; }
-     */
-    public static void getPeak() throws DFTException, IOException {
-        /**
-         * Tone tone = new SineTone(440, (float) 0.95); int time = 5;//ask user
-         * ffor number*
-         */
-        byte[] byteArray = tryStuff();
-        //double[] doubleArray = toDoubleArray(byteArray);
+    public static void getPeak(Tone tone, int numPeak) throws DFTException, IOException {
+
+        byte[] byteArray = getByteArray(tone);
         float[] floatArray = AudioProcessor.byteArrayToFloats(byteArray, 0,
                                                               byteArray.length,
                                                               2);
-        System.out.println(floatArray.length);
         Complex[] input = new Complex[floatArray.length];
-        //System.out.println(doubleArray.length);
+
         for (int i = 0; i < input.length; i++) {
             input[i] = new Complex(floatArray[i], 0);
         }
 
         Complex[] output = computeFFT(input);
-        double magnitude = 0;
-        double freq = 0;
-        for (int i = 0; i < output.length; i = i + 2) {
-            if (output[i].magnitude() > magnitude) {
-                magnitude
-                = output[i].magnitude();
-                freq = i * 44100 / output.length;
-                //http://stackoverflow.com/questions/7674877/how-to-get-frequency-from-fft-result
-            }
-            System.out.println("Peak frequency is: " + freq);
+        double mag = 0;
+        double[] magnitude = new double[output.length / 2];
+        double[] freq = new double[output.length / 2];
+        double peakFreq = 0.0;
+        for (int i = 0; i < output.length / 2; i++) {
+            //System.out.print(output[i].magnitude());
+            /*if (output[i].magnitude() > mag) {
+             mag = output[i].magnitude();
+             peakFreq
+             = i * 44100 / output.length;*/
+
+            //}
+            //System.out.print("stuff");
+            magnitude[i] = output[i].magnitude();
+            //http://stackoverflow.com/questions/7674877/how-to-get-frequency-from-fft-result
 
         }
+        Arrays.sort(magnitude);
+        for (int i = 0; i < magnitude.length / 2; i++) {
+            if (output[i].magnitude() > mag) {
+                mag = output[i].magnitude();
+                peakFreq
+                = i * 44100 / output.length;
+
+            }
+            freq[i] = i * 44100 / output.length;
+        }
+        System.out.print("Peak values are: ");
+        for (int i = 0; i < numPeak; i++) {
+            if (i < magnitude.length) {
+                System.out.print(freq[i] + " Hz ");
+            } else {
+                break;
+            }
+        }
+        System.out.println();
+
+        System.out.println("Peak frequency is: " + peakFreq + " Hz");
     }
+
 }
