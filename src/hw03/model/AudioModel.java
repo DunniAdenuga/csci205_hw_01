a4c97140377b8f1011a6bc5a1013e5522cc27eac
Tim Woodford <tww014@bucknell.edu>
2015-10-28 08:33:23 -0400

    Model for audio waveform

---
+++ src/hw03/model/AudioModel.java
@@ -0,0 +1,83 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: NAMES of team members
+ * Date: 28-Oct-2015
+ * Time: 08:02:51
+ *
+ * Project: csci205_hw_01
+ * Package: hw03.model
+ * File: AudioModel
+ * Description:
+ *
+ * ****************************************
+ */
+package hw03.model;
+
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Tim Woodford
+ */
+public class AudioModel {
+    private WaveForm audioData;
+    private AudioChannel channel;
+
+    public boolean isStereo() {
+        return audioData.getFormat().getChannels() > 1;
+    }
+
+    public double[] getWaveData() throws InvalidModelException {
+        int outLen = audioData.getSampleLength() / (1 + channel.distance);
+        double[] out = new double[outLen];
+        double normalizationFactor = audioData.getSampleSize() == SampleSizeType.EIGHT_BIT ? Byte.MAX_VALUE : Short.MAX_VALUE;
+        if (audioData.getSampleSize() == SampleSizeType.EIGHT_BIT) {
+            ByteBuffer buf = audioData.getByteBufferWrapper();
+            // Move past the channel offset
+            for (int i = 0; i < channel.offset; i++) {
+                buf.get();
+            }
+            for (int sample = 0; sample < outLen; sample++) {
+                out[sample] = buf.get() / normalizationFactor;
+                // Skip over samples that are not from this channel
+                // Does not run after the last channel sample
+                for (int i = 0; i < channel.distance && sample != outLen; i++) {
+                    buf.get();
+                }
+            }
+        } else if (audioData.getSampleSize() == SampleSizeType.SIXTEEN_BIT) {
+            ShortBuffer buf = audioData.getShortBufferWrapper();
+            // Move past the channel offset
+            for (int i = 0; i < channel.offset; i++) {
+                buf.get();
+            }
+            for (int sample = 0; sample < outLen; sample++) {
+                out[sample] = buf.get() / normalizationFactor;
+                // Skip over samples that are not from this channel
+                // Does not run after the last channel sample
+                for (int i = 0; i < channel.distance && sample != outLen; i++) {
+                    buf.get();
+                }
+            }
+        } else {
+            throw new InvalidModelException("Unsupported sample size");
+        }
+        return out;
+    }
+
+    public void setChannel(AudioChannel channel) {
+        this.channel = channel;
+    }
+
+    public List<AudioChannel> getValidChannels() {
+        int distance = isStereo() ? 1 : 0; // Proper distance value for channel
+        return Arrays.stream(AudioChannel.values()).filter(
+                chan -> chan.distance == distance).collect(Collectors.toList());
+    }
+}
