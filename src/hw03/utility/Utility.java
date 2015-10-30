0155b404fcd8a004633c8e829eed9fbb3272baff
Dunni Adenuga <ia005@bucknell.edu>
2015-10-30 00:53:52 -0400

    "doesn't work perfectly, waveComponent is NULL for some reason ?"

---
+++ src/hw03/utility/Utility.java
@@ -0,0 +1,52 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: NAMES of team members
+ * Date: Oct 29, 2015
+ * Time: 6:59:13 PM
+ *
+ * Project: csci205_hw_01
+ * Package: hw03.utility
+ * File: Utility
+ * Description:
+ *
+ * ****************************************
+ */
+package hw03.utility;
+
+import hw03.model.AudioModel;
+import hw03.model.SampleSizeType;
+import hw03.model.WaveForm;
+import hw03.model.WaveFormException;
+import hw03.view.WaveFormComponent;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ *
+ * @author ia005
+ */
+public class Utility {
+
+    public static WaveFormComponent generateWaveFormComponent(float freq,
+                                                              float sampleRate,
+                                                              double length) throws WaveFormException {
+        WaveForm newWave = new WaveForm(freq, sampleRate,
+                                        SampleSizeType.SIXTEEN_BIT, length);
+        return generateWaveFormComponent(newWave);
+    }
+
+    public static WaveFormComponent generateWaveFormComponent(WaveForm newWave) {
+        AudioModel model = new AudioModel();
+        model.setWaveForm(newWave);
+        return new WaveFormComponent(model);
+    }
+
+    public static WaveFormComponent generateWaveFormComponent(File file) throws WaveFormException, UnsupportedAudioFileException, IOException {
+        WaveForm newWave = new WaveForm(file);
+        return generateWaveFormComponent(newWave);
+    }
+
+}
